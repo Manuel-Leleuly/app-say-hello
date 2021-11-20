13653976cdbb8309aa258985a1213f9de2b7a290
Manuel-Leleuly <manuel.leleuly@gmail.com>
2021-11-20 11:53:25 +0700

    feat(project): finish learning importing custom modules

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Manuel-Leleuly/app-say-hello
 
 go 1.17
 
-require github.com/Manuel-Leleuly/go-say-hello v1.0.0 // indirect
+require github.com/Manuel-Leleuly/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"fmt"
 
-	gosayhello "github.com/Manuel-Leleuly/go-say-hello"
+	gosayhello "github.com/Manuel-Leleuly/go-say-hello/v2"
 )
 
 func main() {
-	fmt.Println(gosayhello.SayHello())
+	fmt.Println(gosayhello.SayHello("Manuel"))
 }
